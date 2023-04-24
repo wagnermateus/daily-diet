9f949d97afd943fce76e311d720415b534120416
wagnermateus <wagnermateus7@gmail.com>
2023-04-24 21:21:30 +0100

    Added buttons to meals screen

--- src/screens/Meal/index.tsx
+++ src/screens/Meal/index.tsx
@@ -1,13 +1,28 @@
 import { useState } from "react";
 import { MealHeader } from "../../components/MealHeader";
 import { Container, Content } from "./styles";
+import { Button } from "../../components/Button";
+import { PencilSimpleLine, Trash } from "phosphor-react-native";
+import { useTheme } from "styled-components/native";
 
 export function Meal() {
   const [isOnTheDiet, setIsOnTheDiet] = useState(true);
+  const { COLORS } = useTheme();
   return (
     <Container isOnTheDiet={isOnTheDiet}>
       <MealHeader title="Refeição" />
-      <Content></Content>
+      <Content>
+        <Button
+          title="Editar refeição"
+          icon={<PencilSimpleLine size={18} color={COLORS.WHITE} />}
+        />
+
+        <Button
+          title="Excluir refeição"
+          type="Secondary"
+          icon={<Trash size={18} color={COLORS.gray_100} />}
+        />
+      </Content>
     </Container>
   );
 }
