5d0422a4830b7357983b4bfa92026ce511eb944a
wagnermateus <wagnermateus7@gmail.com>
2023-04-24 21:44:53 +0100

    Finalized the visual structure of the meal screen

--- src/screens/Meal/index.tsx
+++ src/screens/Meal/index.tsx
@@ -1,27 +1,53 @@
 import { useState } from "react";
 import { MealHeader } from "../../components/MealHeader";
-import { Container, Content } from "./styles";
+import {
+  Container,
+  Content,
+  Description,
+  MealInfo,
+  StatusBox,
+  Subtitle,
+  Title,
+} from "./styles";
 import { Button } from "../../components/Button";
 import { PencilSimpleLine, Trash } from "phosphor-react-native";
 import { useTheme } from "styled-components/native";
+import { StatusBall } from "../../components/StatusBall";
+import { View } from "react-native";
 
 export function Meal() {
-  const [isOnTheDiet, setIsOnTheDiet] = useState(true);
+  const [isOnTheDiet, setIsOnTheDiet] = useState(false);
   const { COLORS } = useTheme();
   return (
     <Container isOnTheDiet={isOnTheDiet}>
       <MealHeader title="Refeição" />
       <Content>
-        <Button
-          title="Editar refeição"
-          icon={<PencilSimpleLine size={18} color={COLORS.WHITE} />}
-        />
+        <MealInfo>
+          <Title>Sanduíche</Title>
+          <Description>
+            Sanduíche de pão integral com atum e salada de alface e tomate
+          </Description>
+          <Subtitle>Data e hora</Subtitle>
+          <Description>12/08/2022 às 16:00</Description>
+          <StatusBox>
+            <StatusBall isOnTheDiet={isOnTheDiet} />
+            <Description>
+              {isOnTheDiet ? "dentro da dieta" : "fora da dieta"}
+            </Description>
+          </StatusBox>
+        </MealInfo>
+        <View>
+          <Button
+            title="Editar refeição"
+            icon={<PencilSimpleLine size={18} color={COLORS.WHITE} />}
+          />
 
-        <Button
-          title="Excluir refeição"
-          type="Secondary"
-          icon={<Trash size={18} color={COLORS.gray_100} />}
-        />
+          <Button
+            title="Excluir refeição"
+            type="Secondary"
+            icon={<Trash size={18} color={COLORS.gray_100} />}
+          />
+        </View>
       </Content>
     </Container>
   );
--- src/screens/Meal/styles.ts
+++ src/screens/Meal/styles.ts
@@ -1,5 +1,5 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
 type Props = {
   isOnTheDiet: boolean;
@@ -13,8 +13,45 @@ export const Container = styled(SafeAreaView)<Props>`
 
 export const Content = styled.View`
   flex: 1;
+  justify-content: space-between;
   padding: 40px 24px;
   border-top-left-radius: 20px;
   border-top-right-radius: 20px;
   background-color: ${({ theme }) => theme.COLORS.gray_700};
+`;
+
+export const Title = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.LG}px;
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    color: ${theme.COLORS.gray_100};
+  `}
+`;
+export const Description = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.MD}px;
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+    color: ${theme.COLORS.gray_200};
+  `}
+`;
+export const Subtitle = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.MD}px;
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    color: ${theme.COLORS.gray_100};
+  `}
+`;
+export const MealInfo = styled.View`
+  gap: 24px;
+`;
+export const StatusBox = styled.View`
+  width: 144px;
+  height: 44px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+  background-color: ${({ theme }) => theme.COLORS.gray_600};
+  border-radius: 1000px;
+  padding: 8px 16px;
 `;
